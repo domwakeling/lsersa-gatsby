d69962d7bceb079a177581295263ed10976e75b1
Dom Wakeling <domwakeling@gmail.com>
2023-04-26 22:25:39 +0100

    Update browser-list and add 2011/12 races to yaml

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -17,9 +17,10 @@ const ToDoPage = () => {
                         <li>Training page</li>
                         <li>What training is organised, other than slalom?</li>
                         <li>Add committee, meeting minutes, historical etc</li>
-                        <li>Race results 2011 through 2014 ...</li>
+                        <li>Race results 2013 and 2014 ...</li>
                         <li>Kent schools? Results ... starting 2002, not sure when until (link to results on SkiResults?)</li>
                         <li>London schools? Results ...</li>
+                        <li>Surrey schools? Results ...</li>
                         <li>Confirm race rules policy is OK</li>
                         <li>Any other policies or documents we should add?</li>
                         <li>Can the winter training competition be added? And past years?</li>
