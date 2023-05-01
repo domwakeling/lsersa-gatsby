ae7b5e18fc48b9b1206f0063584bdeadb1a0d9c8
Dom Wakeling <domwakeling@gmail.com>
2023-05-01 22:08:54 +0100

    start testing db connection

---
+++ src/api/dbtest.js
@@ -0,0 +1,16 @@
+import { connect } from '@planetscale/database';
+
+const config = {
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD
+}
+
+export default async function handler(req, res) {
+
+    const conn = await connect(config);
+
+    const results = await conn.execute('SELECT * FROM roles');
+
+    res.status(200).json(results);
+}
---
+++ src/lib/db_refs.js
@@ -0,0 +1,15 @@
+// id values for the roles table
+const roles = {
+    ADMIN: 1,
+    USER: 2
+}
+
+// id values for token_types table
+const tokenTYpes = {
+    PASSWORD_RESET: 1
+}
+
+export {
+    roles,
+    tokenTYpes
+}
