1ea053b7b32823df99ff319a221ff051ffdaad30
Dom Wakeling <domwakeling@gmail.com>
2023-05-12 21:26:42 +0100

    Add blank booking page

---
+++ src/pages/booking.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "../components/Layout.jsx";
+
+const BookingPage = () => {
+    return (
+        <Layout>
+            <div className="container">
+                <div className="row">
+                    <h1 className="underlined">Booking</h1>
+                </div>
+            </div>
+        </Layout>
+
+    )
+}
+
+export default BookingPage;
