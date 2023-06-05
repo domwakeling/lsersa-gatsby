14d9c386e385f0bbe78901099c30b4467568d25f
Dom Wakeling <domwakeling@gmail.com>
2023-06-05 22:13:32 +0100

    Add try/catch to logout

--- src/api/user/logout.js
+++ src/api/user/logout.js
@@ -1,14 +1,21 @@
 export default async function handler(req, res) {
 
-    if (req.method == 'POST') {
-        // set max-age = 0 to effectively delete the cookie
-        res.setHeader("Set-Cookie", `lsersaUserToken=null; Max-Age=0; Path=/`);
-        res.status(200).json({message: "Success"});
-        return;
+    try {
+        if (req.method == 'POST') {
+            // set max-age = 0 to effectively delete the cookie
+            res.setHeader("Set-Cookie", `lsersaUserToken=null; Max-Age=0; Path=/`);
+            res.status(200).json({message: "Success"});
+            return;
+    
+        } else {
+            // method is not POST, fail gracefully
+            res.status(405).json({ message: "ERROR: method not allowed" });
+            return;
+        }
 
-    } else {
-        // method is not POST, fail gracefully
-        res.status(405).json({ message: "ERROR: method not allowed" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: error.message});
         return;
     }
 }
