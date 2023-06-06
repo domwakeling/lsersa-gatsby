d78aaa343f9e5413c3ac0167d9b34b35f5c1375c
Dom Wakeling <domwakeling@gmail.com>
2023-06-06 13:27:46 +0100

    Update booking.jsx

--- src/pages/booking.jsx
+++ src/pages/booking.jsx
@@ -64,10 +64,13 @@ const BookingPage = () => {
 
     const logOutHandler = async (e) => {
         e.preventDefault();
+        const body = {
+            user_email: user.email
+        }
         const res = await fetch(`/api/user/logout`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({user_email: user.email}),
+            body: JSON.stringify(body),
         });
         if (res.status !== 200) {
             const data = await res.json();
