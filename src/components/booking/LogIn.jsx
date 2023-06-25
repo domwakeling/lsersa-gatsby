87c6b29a0fceb271be7c9baa5f94488f2eb4f114
Dom Wakeling <domwakeling@gmail.com>
2023-06-25 17:12:39 +0100

    Tweak presentation on <LogIn />

--- src/components/booking/LogIn.jsx
+++ src/components/booking/LogIn.jsx
@@ -101,8 +101,8 @@ const LogIn = ({ email, setEmail, emailValid, setEmailValid, setMode, setUser })
                     Log In
                 </button>
             </div>
-            <p>If you don't have an account, you need to <Link to="#" onClick={setSignIn}>create one</Link>.</p>
             <p><Link to="#" onClick={setPasswordReset}>Forgotten password</Link>?</p>
+            <p class="larger">If you don't have an account, you need to <Link to="#" onClick={setSignIn}>create one</Link>.</p>
             {loading && <LoadingSpinner />}
             {message !== '' && (
                 <div className="advice-box">
