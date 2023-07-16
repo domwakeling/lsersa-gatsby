48b7474d1dace1593639dd9b3fa472c90599dced
Dom Wakeling <domwakeling@gmail.com>
2023-07-16 15:17:08 +0100

    Testing email errors

--- src/api/admin/send-group-email.js
+++ src/api/admin/send-group-email.js
@@ -85,6 +85,9 @@ export default async function handler(req, res) {
 
             console.log('Retrieved email list:'. emails);
 
+            res.status(200).json({ message: 'successfully sent email' });
+            return;
+
             // send the email
             const _ = await sendLongEmail(emails, subject, subject, htmlArray, textArray);
 
