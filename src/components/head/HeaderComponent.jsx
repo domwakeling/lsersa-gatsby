b7c2e424709e6b04b665ed01138388b6ce79df20
Dom Wakeling <domwakeling@gmail.com>
2023-07-21 09:28:46 +0100

    Fix favicons?

--- src/components/head/HeaderComponent.jsx
+++ src/components/head/HeaderComponent.jsx
@@ -17,7 +17,12 @@ const HeaderComponent = ({ title, description, pathname, children }) => {
             <html lang="en" />
             <meta name="description" content={seo.description} />
             <meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1.0' />
+            <link rel="icon" type="image/x-icon" href="/public/favicon.ico" />
             <link id="favicon" rel="icon" href={fav} sizes="32x32" type="image/png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png"/>
+            {/* <link rel="manifest" href="/public/site.webmanifest"/> */}
             {children}
         </>
     )
