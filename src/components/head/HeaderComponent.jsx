01c1ccbbd55dc5df678a325702f6b0f332709356
Dom Wakeling <domwakeling@gmail.com>
2024-03-19 23:26:42 +0000

    Fix error with favicon paths

--- src/components/head/HeaderComponent.jsx
+++ src/components/head/HeaderComponent.jsx
@@ -17,12 +17,12 @@ const HeaderComponent = ({ title, description, pathname, children }) => {
             <html lang="en" />
             <meta name="description" content={seo.description} />
             <meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1.0' />
-            <link rel="icon" type="image/x-icon" href="/public/favicon.ico" />
+            <link rel="icon" type="image/x-icon" href="/favicon.ico" />
             <link id="favicon" rel="icon" href={fav} sizes="32x32" type="image/png" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png"/>
-            <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png"/>
-            <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png"/>
-            {/* <link rel="manifest" href="/public/site.webmanifest"/> */}
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
             {children}
         </>
     )
